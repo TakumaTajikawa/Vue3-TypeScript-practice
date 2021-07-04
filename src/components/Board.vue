0927c002c584e7a05fa8436f55c501c2f38a6b05
Takuma Tajikawa <>
2021-07-04 23:15:17 +0900

    環境構築の完了

---
+++ src/components/Board.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <header>
+      my Trello
+    </header>
+    <main>
+      <p class="info-line">All: 0 tasks</p>
+      <list-add />
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ListAdd from'./ListAdd.vue'
+
+export default defineComponent({
+  components: { ListAdd },
+  setup() {
+  },
+})
+</script>
---
+++ src/components/ListAdd.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>aaa</h1>
+  <form class="addlist" @submit.prevent="addList">
+    <input v-model="title"
+      type="text"
+      class="text-input"
+      placeholder="Add new list"
+    >
+    <button type="submit" class="add-button">
+      Add
+    </button>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const title = ref('');
+
+    const addList = () => {
+      title.value = ''
+    }
+
+    return {
+      addList,
+    }
+  },
+
+
+
+
+})
+</script>
