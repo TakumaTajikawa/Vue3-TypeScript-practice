51b95a316415d1d74cdf1e9ea130d55d44f790c6
Takuma Tajikawa <tajikawa@arsaga.jp>
2021-07-10 23:39:21 +0900

    一旦コミット

--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -4,6 +4,8 @@
       type="text"
       class="text-input"
       placeholder="Add new list"
+      @focusin="startEditing"
+      @focusout="finishEditing"
     >
     <button type="submit" class="add-button">
       Add
@@ -12,18 +14,40 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
 
 export default defineComponent({
   setup() {
     const title = ref('');
+    const isEditing = ref(false);
 
     const addList = () => {
       title.value = ''
     }
 
+    const startEditing = ():void => {
+      isEditing.value = true
+    }
+
+    const finishEditing = ():void => {
+      isEditing.value = false
+    }
+
+    const classList = computed(() => {
+      let classList = ref('addlist');
+      if (isEditing.value) {
+        classList.value.push('active');
+      }
+      return classList;
+    })
+
+
+
     return {
       addList,
+      startEditing,
+      finishEditing,
+      classList,
     }
   },
   
